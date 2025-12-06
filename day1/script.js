c9afe0128510fdf90e2706c9135dc68e17bf95bf
A-Shushi <219606551+A-Shushi@users.noreply.github.com>
2025-12-06 23:42:52 +0700

    Create a turn right function

--- day1/script.js
+++ day1/script.js
@@ -5,3 +5,11 @@ const data = fs.readFileSync('input.txt', 'utf8');
 
 // Create an array separating each "rotation" from the text file
 const dataArray = data.split(/\r?\n/)
+
+let currentValue = 50;
+
+function turnRight(value) {
+    const reducedValue = value % 100;
+    const sumOfValues = currentValue + reducedValue;
+    return sumOfValues % 100
+}
