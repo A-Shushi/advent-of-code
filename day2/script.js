c0a14e19ca0082283372a66856b32572f6a648a3
A-Shushi <219606551+A-Shushi@users.noreply.github.com>
2025-12-08 14:20:18 +0700

    Create solution function for the problem

--- day2/script.js
+++ day2/script.js
@@ -11,3 +11,25 @@ const formattedArray = dataArray.map(idRange => {
     const idRangeTuple = idRange.split("-");
     return idRangeTuple.map(Number)
 })
+
+function solution(array) {
+    let sumOfIds = 0;
+    array.forEach(tuple => {
+        // Cycle through each number within a given range
+        for (let i = tuple[0]; i <= tuple[1]; i++) {
+            // Check if the length of the number is divisible by 2 so it's symmetrical
+            if (i.toString().length % 2 === 0) {
+                const halfIdLength = i.toString().length / 2;
+                // Compare the left side and the right side of the symmetrical number
+                const leftSide = i.toString().slice(0, halfIdLength);
+                const rightSide = i.toString().slice(halfIdLength);
+                if (leftSide === rightSide) {
+                    sumOfIds += i;
+                }
+            }
+        }
+    })
+    return sumOfIds
+}
+
+console.log(solution(formattedArray))
